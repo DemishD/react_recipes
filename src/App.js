afad874734f539032d3b1f8bf189027006c21039
forrryooou <aaaariadno@gmail.com>
2024-07-01 19:55:46 +0300

    alooo

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React1111
+          Learn React111111
         </a>
       </header>
     </div>
